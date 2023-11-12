1c1e228c3f474fffcfb7e40d569f1fdaec3cbd83
Kewal Kishan Gokuldas <kewal@Kewals-MacBook-Pro.local>
2023-11-12 04:09:42 -0800

    fixing merge issiue

---
+++ dancetok_frontend/BodyDetection/MapViewController.swift
@@ -0,0 +1,133 @@
+//
+//  MapViewController.swift
+//  BodyDetection
+//
+//  Created by Kewal Kishan Gokuldas on 11/12/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController : UIViewController {
+    
+    let mapView : MKMapView = {
+       let map = MKMapView()
+        map.overrideUserInterfaceStyle = .dark
+        return map
+    }()
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setMapConstraints()
+        
+        locationManager.delegate = self
+           locationManager.desiredAccuracy = kCLLocationAccuracyBest
+           locationManager.requestWhenInUseAuthorization()
+        
+        addMultipleAnnotations()
+        centerMapOnLocation()
+        showUserLocation()
+    }
+    
+    func showUserLocation() {
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow // This will follow the user's location
+    }
+    
+    func setMapConstraints(){
+        view.addSubview(mapView)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+
+        
+    }
+    
+    
+    
+    let locations = [
+        (title: "Stanford Uni", latitude: 37.42783889180251, longitude: -122.17006000328483),
+        (title: "Stanford Medical", latitude: 37.4335793391047, longitude: -122.17054284457107),
+        // Add as many locations as you need
+    ]
+    func addMultipleAnnotations() {
+      
+        //37.4335793391047, -122.17054284457107
+     
+
+
+
+        for location in locations {
+            let annotation = MKPointAnnotation()
+            annotation.title = location.title
+            annotation.coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    func centerMapOnLocation() {
+        let coordinate = CLLocationCoordinate2D(latitude: 37.4335793391047, longitude: -122.17054284457107) // Use the coordinate of your annotation
+        let regionRadius: CLLocationDistance = 1000 // in meters
+        let region = MKCoordinateRegion(center: coordinate,
+                                        latitudinalMeters: regionRadius,
+                                        longitudinalMeters: regionRadius)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            switch status {
+                case .authorizedWhenInUse, .authorizedAlways:
+                    if CLLocationManager.locationServicesEnabled() {
+                        locationManager.startUpdatingLocation()
+                    }
+                case .notDetermined:
+                    locationManager.requestWhenInUseAuthorization() // or requestAlwaysAuthorization()
+                case .restricted, .denied:
+                    print("LOCATION DENIED/ Restricted ")
+                    // Location services are not enabled; inform the user
+                    // Show alert telling users how to turn on permissions
+                @unknown default:
+                    break
+            }
+        }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+            calculateDistances(currentLocation: location)
+            
+            
+        }
+        if let location = locations.last {
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+            calculateDistances(currentLocation: location)
+        }
+        
+    }
+    
+    func calculateDistances(currentLocation : CLLocation) {
+
+            print(" calc called ");
+            for marker in locations {
+                let loc =  CLLocation(latitude: marker.latitude, longitude: marker.longitude)
+                let distanceInMeters = currentLocation.distance(from: loc)
+                print("The distance to the marker is \(distanceInMeters) meters.")
+                // Here, you can do something with the distance, like updating the UI
+            }
+        }
+    
+    
+}
--- dancetok_frontend/BodyDetection/ProfileController.swift
+++ dancetok_frontend/BodyDetection/ProfileController.swift
@@ -9,10 +9,82 @@
 import Foundation
 import UIKit
 
-class ProfileController : UIViewController {
+class ProfileController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var compete = ["Nikhil", "Abhay", "Richard", "Abhi"]
+    var songs = ["What is love", "Kinni Kinni" , "Bol Na Halke Halke", "Bhaire Naine"]
+    var dates = ["11/11/2023", "11/10/2023" , "11/10/2023", "11/09/2023"]
+    var firstLabel = ["What is love", "Kinni Kinni" , "Bol Na Halke Halke", "Bhaire Naine"]
+    var secondLabel = ["11/11/2023", "11/10/2023" , "11/10/2023", "11/09/2023"]
+    var thirdLabel = ["", "" , "", ""]
+    var percentage = ["45%", "30%", "60%", "90%"]
+    var blank = ["", "" , "", ""]
+    
+    @IBOutlet weak var competeButton: UIButton!
+    @IBOutlet weak var myStepsButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        competeButton.addTarget(self, action: #selector(competePressed), for: .touchDown)
+        myStepsButton.addTarget(self, action: #selector(mystepsPressed), for: .touchDown)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    @objc func competePressed() {
+        print("compete pressed")
+        firstLabel = compete
+        secondLabel = percentage
+        thirdLabel = songs
+        tableView.reloadData()
+        if #available(iOS 15.0, *) {
+            competeButton.configuration?.baseForegroundColor = .tintColor
+            competeButton.setTitleColor(.tintColor, for: .normal)
+            myStepsButton.configuration?.baseForegroundColor = .label
+            myStepsButton.setTitleColor(.label, for: .normal)
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    @objc func mystepsPressed() {
+        print("my steps pressed")
+        firstLabel = songs
+        secondLabel = dates
+        thirdLabel = blank
+        tableView.reloadData()
+        if #available(iOS 15.0, *) {
+            competeButton.configuration?.baseForegroundColor = .label
+            competeButton.setTitleColor(.label, for: .normal)
+            myStepsButton.configuration?.baseForegroundColor = .tintColor
+            myStepsButton.setTitleColor(.tintColor, for: .normal)
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileTableViewCell") as! ProfileTableViewCell
+        cell.danceLabel.text = firstLabel[indexPath.row]
+        cell.danceDate.text = secondLabel[indexPath.row]
+        cell.dancerLabel.text = thirdLabel[indexPath.row]
+
+        return cell
+    }
+    
+    private func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileTableViewCell") as! ProfileTableViewCell
+
+        // Configure YourCustomCell using the outlets that you've defined.
+
+        return cell
+    }
 }

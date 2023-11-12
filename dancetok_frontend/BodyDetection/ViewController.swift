349fc2318845d2d9fa95dc3c25a4b58e1a3de291
Ferran <ferran.9908@gmail.com>
2023-11-12 04:35:19 -0800

    Checkpoint 2 XD

--- dancetok_frontend/BodyDetection/ViewController.swift
+++ dancetok_frontend/BodyDetection/ViewController.swift
@@ -197,6 +197,7 @@ class ViewController: UIViewController, ARSessionDelegate, RPPreviewViewControll
             }
 
             // Handle the response here
+            
         }
         task.resume()
     }
